3c7433f53411c1e016ff7ceb8001a85c401a494f
Daniel Ribeiro dos Santos <ribeiro946@gmail.com>
2021-08-24 22:01:00 +0100

    Time measurement

--- Recepcao/TCC/Binaire/Serial/Reception_Serial.py
+++ Recepcao/TCC/Binaire/Serial/Reception_Serial.py
@@ -2,22 +2,31 @@ import serial
 import time
 import sys
 
-ser = serial.Serial('/dev/ttyS0',9600)
+ser = serial.Serial('/dev/ttyS0',2400)
 TARGETSIZE = 10
 
 size = ser.read(TARGETSIZE)
+
+time1 = time.time()
 
 size = int(size)
 
 data = ser.read(size)
 
-print(len(data))
+print(str(len(data)) + " bytes \n")
 
 ser.close()
+
+print("Transmission time = %s seconds\n" %(time.time()-time1))
+
+time1 = time.time()
 
 fh = open("Received_Image.jpeg","wb")
 
 fh.write(data)
 fh.close()
+
+print("Writing time = %s seconds" %(time.time()-time1))
+
 
 
