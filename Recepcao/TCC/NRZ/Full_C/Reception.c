f692ec8e60a5055b98c300dcad389892c89937bf
Daniel Ribeiro dos Santos <ribeiro946@gmail.com>
2021-12-11 12:24:06 -0300

    BER Reception
    BER for NRZ Reception Code

--- Recepcao/TCC/NRZ/Full_C/Reception.c
+++ Recepcao/TCC/NRZ/Full_C/Reception.c
@@ -76,9 +76,88 @@ int main()
 	fclose(file_write);
 
 
-	printf("\n\nEnd of Program\n");
 	gpioTerminate();
 	}
+
+
+
+	// ---------------------------------- BER ----------------------------------
+
+	FILE *file_read;
+	FILE *file_original;
+	file_read = fopen("Received_Text.txt","r");
+	file_original = fopen("Doc1.txt","r");
+
+	fseek(file_read,0,SEEK_END);  //Get the size of the file
+	int size = ftell(file_read); // Ignore the last element (end of file)
+	printf("\nSize = %d \n", size);
+	rewind(file_read);
+
+
+	int val[size];
+	_Bool bin[size][8];
+	int original_file[size];
+	_Bool original_file_bin[size][8];
+
+
+    for (int i = 0; i < size ; i++){
+        val[i] = (int)fgetc(file_read);
+		original_file[i] = (int)fgetc(file_original); 
+		// printf("%d ",original_file[i]);
+        // printf("%d ",val[i]);
+    }
+
+    fclose(file_read);
+	fclose(file_original);
+
+    printf("\n\n");
+
+
+    for (int i = 0; i < size; i++){
+        for(int j = 0; j < 8; j++){
+            if(j ==0){
+				bin[i][j] = val[i]/128; 
+				original_file_bin[i][j] = original_file[i]/128;
+				resto = val[i]%128;
+				resto_original = original_file[i]%128;
+				}
+            else{
+                potencia = pow(2,7-j);
+                bin[i][j] = resto/(potencia);
+				original_file_bin[i][j] = resto_original/(potencia);
+                resto = resto%potencia;
+				resto_original = resto_original%potencia;
+            }
+           printf("%d",bin[i][j]);
+		   if(bin[i][j] != original_file_bin[i][j])
+		   		BER_bit++;
+        }
+       printf("  ");
+    }
+
+	printf("\n \n");
+
+	for (int i = 0; i < size; i++){
+		for (int j = 0; j < 8; j++){
+			printf("%d",original_file_bin[i][j]);
+		}
+		printf("  ");
+	}
+
+	printf("\n\n");
+	BER = (double)BER_bit/(double)size;
+
+	printf("BER = %d/%d = %f \n",BER_bit, size, BER);
+
+ 
+	printf("\n");
+
+
+	printf("\n\nEnd of Program\n");
+
+
+
+
 }
 
 
